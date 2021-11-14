87ea0c0296c0e2b3836ed5b3f05b6de9ea9dedb2
Francesco Pettrone <>
2021-11-14 15:48:18 +0100

    removed client vue application

--- client/src/services/Api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-//object to store the basic app connection
-export default () => {
-    return axios.create({
-      baseURL: `http://localhost:8081/`,
-    })
-  }
--- client/src/services/AuthenticationService.js
+++
@@ -1,8 +0,0 @@
-import Api from "./Api";
-
-//this method post the credentials to the register endpoint on the express server
-export default {
-    register(credentials) {
-        return Api().post('register',credentials)
-    }
-}
